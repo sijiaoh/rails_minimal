ae18f2f48451c227f5ab615ed5700431a3977abf
sijiaoh <sijiaoh@outlook.com>
2023-12-30 05:58:11 +0900

    Add docker_postgres command.

---
+++ lib/backends/docker_postgres.rb
@@ -0,0 +1,45 @@
+class DockerPostgres < Thor
+  desc "up", "docker run --rm"
+  def up
+    stop
+    exec "docker run --rm #{opts}"
+  end
+
+  desc "start", "docker run --detach"
+  def start
+    system "docker run --detach #{opts}", exception: true
+  end
+
+  desc "stop", "docker rm --force"
+  def stop
+    system "docker rm --force #{name}", exception: true
+  end
+
+  desc "rm", "docker rm --force --volumes"
+  def rm
+    system "docker rm --force --volumes #{name}", exception: true
+  end
+
+  private
+
+  def opts
+    [
+      "--name #{name}",
+      "--env POSTGRES_USER='#{user}' --env POSTGRES_PASSWORD='#{password}'",
+      "--volume #{volume}:/var/lib/postgresql/data",
+      "--publish #{port}:5432",
+      image
+    ].join(" ")
+  end
+
+  def project_name = Rails.application.class.module_parent_name.underscore
+  def name = "#{project_name}_postgres"
+  def db_conf = Rails.configuration.database_configuration[Rails.env]
+  def user = db_conf["username"]
+  def password = db_conf["password"]
+  def port = db_conf["port"]
+  def volume = name
+  def image = "postgres:16"
+end
+
+DockerPostgres.start(ARGV)
